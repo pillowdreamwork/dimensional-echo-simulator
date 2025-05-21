4dc8c75b3008673cf35a7963de544e850bb39806
pillowdreamwork <mgmanishgarg3@gmail.com>
2025-05-21 07:21:16 +0000

    Add system check utility to validate engine module statuses

---
+++ src/utils/moduleCheck.ts
@@ -0,0 +1,38 @@
+import { getEngineModules } from '../lib/engine';
+
+export function runSystemCheck() {
+  const modules = getEngineModules();
+  const checks = {
+    coreRunning: !!modules.simulationCore,
+    dreamworkActive: !!modules.pillowDreamwork,
+    vectorAlchemyReady:
+      !!modules.vectorAlchemy &&
+      typeof modules.vectorAlchemy.isReady === 'function' &&
+      modules.vectorAlchemy.isReady(),
+    ritualSystemOnline:
+      !!modules.iuri &&
+      typeof modules.iuri.isInitialized === 'function' &&
+      modules.iuri.isInitialized(),
+    mythicIntelligenceConnected:
+      !!modules.mythicAI &&
+      typeof modules.mythicAI.isConnected === 'function' &&
+      modules.mythicAI.isConnected(),
+    uncertaintyEngineCalibrated:
+      !!modules.uncertainty &&
+      typeof modules.uncertainty.isCalibrated === 'function' &&
+      modules.uncertainty.isCalibrated(),
+    echoSimulatorFunctional:
+      !!modules.echoSimulator &&
+      typeof modules.echoSimulator.isOperational === 'function' &&
+      modules.echoSimulator.isOperational(),
+    dreamServerOnline:
+      !!modules.dreamServer &&
+      typeof modules.dreamServer.isOnline === 'function' &&
+      modules.dreamServer.isOnline(),
+    siderAIResponsive:
+      !!modules.siderAI &&
+      typeof modules.siderAI.isResponsive === 'function' &&
+      modules.siderAI.isResponsive(),
+  };
+  return checks;
+}
